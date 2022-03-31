c4aa230a552a8236a17c92c936210b8b2e2a8ac4
akhil kishor <b.akhil072@gmail.com>
2022-03-31 16:39:39 +0530

    trying for new one to make sure the static files are workng or not

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -201,12 +201,11 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'static')
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
